41d476dcc5716648dab853ff2802fa03fa82a542
Henrique de melo <henrique.de.melo.cristioglu@gmail.com>
2020-06-05 15:42:26 -0300

    style: responsive home title schedule

--- src/pages/Home/components/Schedule/index.tsx
+++ src/pages/Home/components/Schedule/index.tsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import './style.css'
 
 const Schedule: React.FC = () => {
   return (
-    <div>
-
+    <div className="wrapper_schedules">
+      <div className="wrapper_title">
+        <h3>Agenda</h3>
+      </div>
+      <div className="row">
+        <div className="row_schedules">
+          <div className="card">
+            <div className="content">
+              <h4>Fevereiro</h4>
+              <p>Corrida livre e rápida</p>
+              <div className="row">
+                <span>Dia: 02</span>
+                <span>Horário: 18:20</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Introduction from './components/introduction'
+import Schedule from './components/Schedule'
 
 import './style.css'
 
@@ -8,23 +9,7 @@ const Home: React.FC = () => {
     <div className="wrapper_home">
       <div className="container">
         <Introduction />
-        <div className="wrapper_schedules">
-          <div className="wrapper_title">
-            <h3>Agenda</h3>
-          </div>
-          <div className="row_schedules">
-            <div className="card">
-              <div className="content">
-                <h4>Fevereiro</h4>
-                <p>Corrida livre e rápida</p>
-                <div className="row">
-                  <span>Dia: 02</span>
-                  <span>Horário: 18:20</span>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
+        <Schedule />
       </div>
     </div>
   )
