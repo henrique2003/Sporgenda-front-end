6984e25e963627358c0d2070bec73d107ab1db7a
Henrique de melo <henrique.de.melo.cristioglu@gmail.com>
2020-06-05 15:34:13 -0300

    refactor: move itroduction home to a single file

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Sport from '../../assets/images/sport-1.png'
+import Introduction from './components/introduction'
 
 import './style.css'
 
@@ -7,15 +7,22 @@ const Home: React.FC = () => {
   return (
     <div className="wrapper_home">
       <div className="container">
-        <div className="row">
-          <div className="warapper_text">
-            <div className="content">
-              <h1>Querendo se exercitar ?</h1>
-              <p>Escolha qualquer horário e vá praticar esportes com outras pessoas. É de graça e sem qualquer restrição</p>
-            </div>
+        <Introduction />
+        <div className="wrapper_schedules">
+          <div className="wrapper_title">
+            <h3>Agenda</h3>
           </div>
-          <div className="wrapper_image">
-            <img src={Sport} alt=""/>
+          <div className="row_schedules">
+            <div className="card">
+              <div className="content">
+                <h4>Fevereiro</h4>
+                <p>Corrida livre e rápida</p>
+                <div className="row">
+                  <span>Dia: 02</span>
+                  <span>Horário: 18:20</span>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
